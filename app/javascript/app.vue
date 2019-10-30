667b5fc69be7223a091f7f00cfaa18224385e4c7
ctp1204 <lqcuong.qt@gmail.com>
2019-10-30 16:58:50 +0700

    first commit

---
+++ app/javascript/app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <p>{{ message }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        message: "Hello Vue!12121"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  p {
+    font-size: 60px;
+    text-align: center;
+    color: green;
+    font-weight: bold;
+  }
+</style>
